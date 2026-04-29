848dbacb6b4c9b8af758f4398f936e1fc7a40d85
Sandeep <sndpgauti@gmail.com>
2026-04-29 18:37:30 +0530

    Arrays updated

--- Arrays/11_Majority.py
+++ Arrays/11_Majority.py
@@ -22,3 +22,45 @@ def majority(arr):
 
 arr=[1,2,2,2,2,2,4,5,6]
 print(majority(arr))
+
+
+'''
+def majority_element(arr):
+    freq = {}
+    n = len(arr)
+
+    for num in arr:
+        freq[num] = freq.get(num, 0) + 1
+
+        if freq[num] > n // 2:
+            return num
+
+    return None  # if no majority element exists
+
+
+# Example
+arr = [2, 2, 1, 2, 3, 2, 2]
+print(majority_element(arr))
+
+'''
+
+'''
+def most_repeating(arr):
+    freq = {}
+
+    # Count frequency
+    for i in arr:
+        freq[i] = freq.get(i, 0) + 1
+
+    # Find element with maximum frequency
+    max_count = 0
+    result = -1
+
+    for key in freq:
+        if freq[key] > max_count:
+            max_count = freq[key]
+            result = key
+
+    return result
+
+'''
